be783eccd1063f98a217dcccbe3083b509d0b80e
pablo-kachoski <pablo.kachoski@appdirect.com>
2017-02-10 18:40:05 -0300

    Css styles E2E

--- src/components/Apps/e2e/components/RegressionTest.js
+++ src/components/Apps/e2e/components/RegressionTest.js
@@ -16,7 +16,7 @@ class RegressionTest extends React.Component {
 				'color': '#BD5D1D'
 			},
 			running: {
-				'color': '#FFCA28'
+				'color': 'rgb(255, 182, 40)'
 			},
 			failure: {
 				'color': 'red'
@@ -47,46 +47,70 @@ class RegressionTest extends React.Component {
 	render() {
 		let styles = {
 			container: {
-				'margin': '20px',
-				'padding': '5px',
-				'border-left': '5px solid blue'
+				margin: '20px 0px',
+				padding: '15px',
+				borderTop: '15px solid rgba(0, 0, 125, 0.3)',
+				borderRadius: '5px',
+				backgroundColor: 'rgba(160, 195, 255, 0.22)'
 			},
 			details: {
-				'border-left': '1px solid gray',
-				'padding': '10px',
-				'margin-left': '5px',
-				'color': '#808080'
+				
+				color: 'black'
 			},
-			title: {
-				'font-size': '18px',
-				'color': 'black',
-				'margin-right': '5px'
+			title: {	
+				fontSize: '18px',
+				color: 'black',
+				width: '70px',
+			    display: 'inline-block',
+			    backgroundColor: 'rgba(155,155,155,0.5)',
+			    padding: '5px 15px',
+			    lineHeight: '20px'
+			},
+			dataColumn: {
+				position: 'absolute',
+				left: '100px',
+				padding: '5px 15px',
+				lineHeight: '20px',
+			    backgroundColor: 'rgba(255,255,255,1)',
+			    width: '550px',
+			    
+			    whiteSpace: 'nowrap',
+				overflow: 'hidden',
+				textOverflow: 'ellipsis',
+			},
+			dataContainer: {
+				position: 'relative',
+				marginBottom: '1px'
 			}
 		}
-		console.log(this.props.regression);
 		let link = "https://e2e.appdirect.tools/run/" + this.props.regression.id;
 		return (
 			<div style={styles.container}>
-				<h3><strong>Name:</strong>{this.props.regression.appDirectVersion}</h3>
+				<h3>{this.props.regression.appDirectVersion}</h3>
 				<div style={styles.details}>
-					<h4>
-						<span style={styles.title}>Suite:</span>
-						{this.props.regression.suite}</h4>
-					<h4>
-						<span style={styles.title}>Duration:</span>
-						{this.props.regression.duration}</h4>
-					<h4>
-						<span style={styles.title}>Date:</span>
-						{this.props.regression.created_at}</h4>
-					<h4>
-						<span style={styles.title}>Name:</span>
-						{this.props.regression.createdBy ? this.props.regression.createdBy.profile.name : 'Master'}</h4>
-					<h4>
-						<span style={styles.title}>Status:</span>
-						{this.getIconE2E()}</h4>
-					<h4>
-						<span style={styles.title}>Link:</span>
-						<a href={link} target="_blank">{this.props.regression.id}</a>
+					<h4 style={styles.dataContainer}>
+						<span style={styles.title}>Suite</span>
+						<span style={styles.dataColumn}>{this.props.regression.suite}</span>
+					</h4>
+					<h4 style={styles.dataContainer}>
+						<span style={styles.title}>Duration</span>
+						<span style={styles.dataColumn}>{this.props.regression.duration}</span>
+					</h4>
+					<h4 style={styles.dataContainer}>
+						<span style={styles.title}>Date</span>
+						<span style={styles.dataColumn}>{this.props.regression.created_at}</span>
+					</h4>
+					<h4 style={styles.dataContainer}>
+						<span style={styles.title}>Name</span>
+						<span style={styles.dataColumn}>{this.props.regression.createdBy ? this.props.regression.createdBy.profile.name : 'Master'}</span>
+					</h4>
+					<h4 style={styles.dataContainer}>
+						<span style={styles.title}>Status</span>
+						<span style={styles.dataColumn}>{this.getIconE2E()}</span>
+					</h4>
+					<h4 style={styles.dataContainer}>
+						<span style={styles.title}>Link</span>
+						<a style={styles.dataColumn} href={link} target="_blank">{this.props.regression.id}</a>
 					</h4>
 				</div>
 			</div>
