6f3265a8ce974397fa1a4b28691612e9e46b0046
Angel <Angel@Angels-MacBook-Pro.local>
2017-02-10 13:04:41 -0300

    add home link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,78 +1,78 @@
 import React from 'react';
 import Avatar from 'material-ui/Avatar';
 
-const style = { margin: 5 };
+const style = {margin: 5};
 
 class Header extends React.Component {
-    constructor() {
-        super();
-        this.renderAuthHeader = this.renderAuthHeader.bind(this);
-        this.renderNoAuthHeader = this.renderNoAuthHeader.bind(this);
-        this.renderLogo = this.renderLogo.bind(this);
-    }
+	constructor() {
+		super();
+		this.renderAuthHeader = this.renderAuthHeader.bind(this);
+		this.renderNoAuthHeader = this.renderNoAuthHeader.bind(this);
+		this.renderLogo = this.renderLogo.bind(this);
+	}
 
-    renderLogo() {
-        return (
-            <div className="logo">
-                <h2><span className="iconLogo"></span>Valkyrie!</h2>
-            </div>
-        );
-    };
+	renderLogo() {
+		return (
+			<div className="logo">
+				<h2><a href="/" className="linklogo"><span className="iconLogo"></span>Valkyrie!</a></h2>
+			</div>
+		);
+	};
 
-    renderAuthHeader() {
-        return (
-            <header className="header">
-                <div className="headerContainer">
-                    {this.renderLogo()}
-                    <div className="searchBox">
+	renderAuthHeader() {
+		return (
+			<header className="header">
+				<div className="headerContainer">
+					{this.renderLogo()}
+					<div className="searchBox">
 
-                    </div>
-                    <div className="manage">
-                        <h3> <span>
+					</div>
+					<div className="manage">
+						<h3> <span>
                             <Avatar
-                                //DEG: I have a problem with the picture. This URL return an 403 error.
-                                src={localStorage.getItem('picture')}
-                                size={45}
-                                style={style}
-                            />
+								//DEG: I have a problem with the picture. This URL return an 403 error.
+								src={localStorage.getItem('picture')}
+								size={45}
+								style={style}
+							/>
                         </span>
-                            {this.props.Profile.Name}
-                        </h3>
-                    </div>
-                </div>
-            </header>
-        );
-    }
+							{this.props.Profile.Name}
+						</h3>
+					</div>
+				</div>
+			</header>
+		);
+	}
 
-    renderNoAuthHeader() {
-        return (
-            <nav className="header">
-                <div className="headerContainer">
-                    {this.renderLogo()}
-                    <div className="searchBox">
-                        <h2></h2>
-                    </div>
-                    <div className="manage">
-                        <h2>Login</h2>
-                    </div>
-                </div>
-            </nav>
-        );
-    }
+	renderNoAuthHeader() {
+		return (
+			<nav className="header">
+				<div className="headerContainer">
+					{this.renderLogo()}
+					<div className="searchBox">
+						<h2></h2>
+					</div>
+					<div className="manage">
+						<h2>Login</h2>
+					</div>
+				</div>
+			</nav>
+		);
+	}
 
-    render() {
-        var IsAuth = this.props.isAuth || false;
-        if (IsAuth) {
-            return (this.renderAuthHeader());
-        } else {
-            return this.renderNoAuthHeader();
-        }
-    }
+	render() {
+		var IsAuth = this.props.isAuth || false;
+		if (IsAuth) {
+			return (this.renderAuthHeader());
+		} else {
+			return this.renderNoAuthHeader();
+		}
+	}
 }
 
 Header.propTypes = {
-    isAuth: React.PropTypes.bool.isRequired,
-    Profile: React.PropTypes.object.isRequired
+	isAuth: React.PropTypes.bool.isRequired,
+	Profile: React.PropTypes.object.isRequired
 };
 
 
