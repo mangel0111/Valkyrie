5e10702a031157bef1f3a5ca64e38feb1f97e12b
Angel <Angel@Angels-MacBook-Pro.local>
2017-05-11 16:39:06 -0300

    add rating

--- src/components/Apps/skills/components/Skills.js
+++ src/components/Apps/skills/components/Skills.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import Avatar from 'material-ui/Avatar';
 import HeaderProfile from './HeaderProfile';
+import Rating from './Rating';
 
 class Skills extends React.Component {
 
 	constructor() {
 		super();
 		this.state = {
-			events: []
+			events: [],
+			habilities: [{
+				id: 1,
+				name: "React",
+				value: 3
+			}]
 		};
 	}
+
+	changeInput(value, hability) {
+		let habilities = this.state.habilities;
+		habilities.forEach((h) => {
+			if (h.id = hability.id) {
+				h.value = value
+			}
+		});
+		this.setState({habilities});
+	}
+
+	showHabilities(hability) {
+		return (<Rating key={hability.id} hability={hability} onChangeInput={(value, hability) => this.changeInput(value, hability)}/>);
+	}
+
 	render() {
 		return (
 			<div>
 				<HeaderProfile/>
+				{this.state.habilities.map((hability) => this.showHabilities(hability))}
 			</div>
 		);
 	}
