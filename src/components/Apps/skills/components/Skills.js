16a3dd45205a4d24c912d92b387eb5926f9c6a59
Angel <Angel@Angels-MacBook-Pro.local>
2017-05-12 13:11:53 -0300

    add fucntionality to manage skills

--- src/components/Apps/skills/components/Skills.js
+++ src/components/Apps/skills/components/Skills.js
@@ -10,6 +10,7 @@ class Skills extends React.Component {
 		super();
 		this.state = {
 			events: [],
+			addSkill: false,
 			skills: [{
 				id: 1,
 				name: "React",
@@ -25,7 +26,8 @@ class Skills extends React.Component {
 				name: "SQL",
 				rating: 2,
 				code: 'sql'
-			}]
+			}],
+			nextSkill: ''
 		};
 	}
 
@@ -40,18 +42,54 @@ class Skills extends React.Component {
 		this.setState({skills});
 	}
 
+	removeSkill(skillId) {
+		let skills = this.state.skills;
+		let indexToRemove = null;
+		skills.forEach((h, index, object) => {
+			if (h.id === skillId) {
+				object.splice(index, 1);
+			}
+		});
+		this.setState({skills});
+	}
+
 	showSkill(skill) {
 		return (
 			<div className="ratingContainer" key={skill.code}>
-				<h3>{skill.name}</h3>
+				<h3>{skill.name}<a onClick={() => this.removeSkill(skill.id)}>x</a></h3>
 				<Rating
 					empty="fa fa-star-o fa-2x"
 					full="fa fa-star fa-2x"
 					fractions={2}
 					initialRate={skill.rating}
-					onChange={(rate) => {debugger;this.changeInput(rate, skill);}}
+					onChange={(rate) => this.changeInput(rate, skill)}
 				/>
 			</div>);
+	}
+
+	addSkill() {
+		let skills = this.state.skills;
+
+		skills.push({
+			name: this.state.nextSkill,
+			rating: 0,
+			code: this.state.nextSkill
+		});
+		this.setState({
+				skills,
+				addSkill: false,
+				nextSkill: null
+			});
+	}
+
+	handleChange(e) {
+		this.setState({nextSkill: e.target.value});
+	}
+
+	requestSkill() {
+		this.setState({
+			addSkill: true
+		})
 	}
 
 	render() {
@@ -59,6 +97,12 @@ class Skills extends React.Component {
 			<div>
 				<HeaderProfile/>
 				{this.state.skills.map((skill) => this.showSkill(skill))}
+				{
+					this.state.addSkill ? (<div className="addSkill">
+							<input type="text" value={this.state.nextSkill} onChange={(e) => this.handleChange(e)}/><a onClick={() => this.addSkill()} className="btn btn-custom">+</a>
+						</div>) : null
+				}
+				<button className="btn btn-custom" onClick={() => this.requestSkill()}>Add Skill</button>
 				<Navigation/>
 			</div>
 		);
